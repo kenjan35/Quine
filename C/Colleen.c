32609ed2eccaa904b7e876cc898211fe8053659c
Andriniaina Fernand Tolojanahary <atolojan@c1r8s11.42antananarivo.mg>
2026-04-22 13:19:56 +0300

    Fix char *s of Colleen

--- C/Colleen.c
+++ C/Colleen.c
@@ -5,7 +5,7 @@
 */
 void	print_quine(void)
 {
-	char	*s = "#include <stdio.h>%c%c/*%c%cThis function will print the source code%c*/%cvoid%cprint_quine(void)%c{%c%cchar%c*s = %c%s%c;%c%cprintf(s, 10, 10, 10, 9, 10, 10, 9, 9, 34, s, 34,	10, 9, 10, 10, 10, 9, 10, 10, 9, 10, 9, 10, 9, 10, 10, 10, 10);%c}%c%cint%cmain(void)%c{%c%c/*%c%c%cThis function is inside%c%c*/%c%cprint_quine();%c%creturn (0);%c}%c";
+	char	*s = "#include <stdio.h>%c%c/*%c%cThis function will print the source code%c*/%cvoid%cprint_quine(void)%c{%c%cchar%c*s = %c%s%c;%c%cprintf(s, 10, 10, 10, 9, 10, 10, 9, 10, 10, 9, 9, 34, s, 34,	10, 9, 10, 10, 10, 9, 10, 10, 9, 10, 9, 9, 10, 9, 10, 9, 10, 9, 10, 10, 10, 10);%c}%c%cint%cmain(void)%c{%c%c/*%c%c%cThis function is inside%c%c*/%c%cprint_quine();%c%creturn (0);%c}%c";
 	printf(s, 10, 10, 10, 9, 10, 10, 9, 10, 10, 9, 9, 34, s, 34,	10, 9, 10, 10, 10, 9, 10, 10, 9, 10, 9, 9, 10, 9, 10, 9, 10, 9, 10, 10, 10, 10);
 }
 
