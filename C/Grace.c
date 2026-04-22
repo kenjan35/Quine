464281d462a964abf14b0ae2cfc6744ab2729ec9
Andriniaina Fernand Tolojanahary <atolojan@c1r8s11.42antananarivo.mg>
2026-04-22 13:41:50 +0300

    Add comment in Grace

--- C/Grace.c
+++ C/Grace.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
+/*
+	This is a comment
+*/
 #define	FILENAME "Grace_kid.c"
-#define	CODE "#include <stdio.h>%c%c#define%cFILENAME %cGrace_kid.c%c%c#define%cCODE %c%s%c%c#define%cFT(x) int%cmain(){ x }%c%cFT(FILE%c*f = fopen(FILENAME, %c%c%c); fprintf(f, CODE, 10, 10, 9, 34, 34, 10, 9, 34, CODE, 34, 10, 9, 9, 10, 10, 9, 34, 119, 34, 10); fclose(f); return (0);)%c"
+#define	CODE "#include <stdio.h>%c%c/*%c%cThis is a comment%c*/%c#define%cFILENAME %cGrace_kid.c%c%c#define%cCODE %c%s%c%c#define%cFT(x) int%cmain(){ x }%c%cFT(FILE%c*f = fopen(FILENAME, %c%c%c); fprintf(f, CODE, 10, 10, 10, 9, 10, 10, 9, 34, 34, 10, 9, 34, CODE, 34, 10, 9, 9, 10, 10, 9, 34, 119, 34, 10); fclose(f); return (0);)%c"
 #define	FT(x) int	main(){ x }
 
-FT(FILE	*f = fopen(FILENAME, "w"); fprintf(f, CODE, 10, 10, 9, 34, 34, 10, 9, 34, CODE, 34, 10, 9, 9, 10, 10, 9, 34, 119, 34, 10); fclose(f); return (0);)
+FT(FILE	*f = fopen(FILENAME, "w"); fprintf(f, CODE, 10, 10, 10, 9, 10, 10, 9, 34, 34, 10, 9, 34, CODE, 34, 10, 9, 9, 10, 10, 9, 34, 119, 34, 10); fclose(f); return (0);)
